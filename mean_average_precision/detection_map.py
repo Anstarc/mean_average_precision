847e403d490452f9346981492b2a69d0d5c62ec9
Anstarc <lichunpeng@cibr.ac.cn>
2025-07-08 16:12:55 +0800

    change len(pred_bb) to pred_bb.size when pred_bb is repeated

--- mean_average_precision/detection_map.py
+++ mean_average_precision/detection_map.py
@@ -50,7 +50,7 @@ class DetectionMAP:
         if pred_bb.ndim == 1:
             pred_bb = np.repeat(pred_bb[:, np.newaxis], 4, axis=1)
         IoUmask = None
-        if len(pred_bb) > 0 and len(gt_bb) > 0:
+        if pred_bb.size > 0 and gt_bb.size > 0:
             IoUmask = self.compute_IoU_mask(pred_bb, gt_bb, self.overlap_threshold)
         for accumulators, r in zip(self.total_accumulators, self.pr_scale):
             if DEBUG:
